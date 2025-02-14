c74d6d90e1fc5f9c3f87662797697b86a4bb9a16
johnchd <jcrynick@gmai.com>
2025-02-14 11:18:56 -0500

    test-commit-1

--- minimalapi-1/obj/Debug/net9.0/MinimalApi.AssemblyInfo.cs
+++ minimalapi-1/obj/Debug/net9.0/MinimalApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3225f0af2e6605f5ee5b80b2f14168b72ace927c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0c7050da769507763454a5c2821595ecce3b13")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
