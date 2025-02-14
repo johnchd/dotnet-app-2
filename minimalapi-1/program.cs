61fcaf9fd47f3404714931a3b0aae1207b0a724f
John C <84040540+johnchd@users.noreply.github.com>
2025-02-14 11:06:35 -0500

    Add files via upload

---
+++ minimalapi-1/program.cs
@@ -0,0 +1,24 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
+
+app.MapGet("/greeting", () => "aws w/ .NET 9 ---test1");
+
+
+//defines POST
+app.MapPost("/message", async (HttpRequest request) =>
+{
+    // reads JSON body from the HTTP request
+    // then converts the JSON string into a C# string using ReadFromJsonAsync<string>() method
+    string message = await request.ReadFromJsonAsync<string>();
+    return $"Received: {message}";
+});
+
+//defines PUT
+app.MapPut("/message", async (HttpRequest request) =>
+{
+    string message = await request.ReadFromJsonAsync<string>();
+    return $"Updated to: {message}";
+});
+
+
+app.Run("http://*:80");
